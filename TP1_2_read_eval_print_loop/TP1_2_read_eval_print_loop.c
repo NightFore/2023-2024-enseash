126cc9aaed4a1d548cffc7be5cbdf83757f65676
NightFore <Et3rnNighTaLL@Hotmail.com>
2023-11-27 09:01:29 +0100

    Exercice 2-1

--- TP1_2_read_eval_print_loop/TP1_2_read_eval_print_loop.c
+++ TP1_2_read_eval_print_loop/TP1_2_read_eval_print_loop.c
@@ -1,52 +1,49 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAX_INPUT_SIZE 100
 
 void displayWelcomeMessage() {
     const char welcomeMessage[] = "Welcome to ENSEA Tiny Shell.\n"
                                   "Type 'exit' to quit.\n";
+
+    // Write the welcome message to the standard output
     write(STDOUT_FILENO, welcomeMessage, sizeof(welcomeMessage) - 1);
 }
 
 void displayPrompt() {
     const char prompt[] = "enseash % ";
+
+    // Write the shell prompt to the standard output
     write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 }
 
 ssize_t readPrompt(char *input, size_t size) {
+    // Read input from standard input
     ssize_t bytesRead = read(STDIN_FILENO, input, sizeof(input));
 
-    if (bytesRead <= 0) {
-        write(STDOUT_FILENO, "Error reading input.\n", 21);
+    // Check for errors
+    if (bytesRead < 0) {
+        const char errorInputMessage[] = "Error: readPrompt.\n";
+        write(STDOUT_FILENO, errorInputMessage, sizeof(errorInputMessage) - 1);
         exit(EXIT_FAILURE);
     }
 
-    // Remove trailing newline character
-    input[bytesRead - 1] = '\0';
-
+    // Return input
     return bytesRead;
 }
 
 int main() {
     char input[MAX_INPUT_SIZE];
 
+    // Display the welcome message at the beginning
     displayWelcomeMessage();
 
     while (1) {
+        // Display the shell prompt and read user input
         displayPrompt();
-        ssize_t bytesRead = readPrompt(input, sizeof(input));
-
-        if (strcmp(input, "exit") == 0) {
-            write(STDOUT_FILENO, "Exiting ENSEA Tiny Shell. Goodbye!\n", 34);
-            break;
-        } else {
-            write(STDOUT_FILENO, "Executing command: ", 19);
-            write(STDOUT_FILENO, input, bytesRead - 1);
-            write(STDOUT_FILENO, "\n", 1);
-        }
+        readPrompt(input, sizeof(input));
     }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
